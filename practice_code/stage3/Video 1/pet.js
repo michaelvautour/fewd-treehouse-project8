845b10747d714aa6b5376297af5527f673fefa30
Michael Vautour <michael.e.vautour@gmail.com>
2021-01-30 14:50:12 -0700

    dot notation to call a new method created

--- practice_code/stage3/Video 1/pet.js
+++ practice_code/stage3/Video 1/pet.js
@@ -1,13 +1,22 @@
 class Pet {
-    constructor(animal, age, breed) {
+    constructor(animal, age, breed, sound) {
         this.animal = animal;
         this.age = age;
         this.breed = breed;
+        this.sound = sound;
     }
+
+    speak() {
+        console.log(this.sound);
+    }
+
 }
 
-const ernie = new Pet('dog', 2, 'pug');
-const vera = new Pet('dog', 7, 'border collie');
+const ernie = new Pet('dog', 2, 'pug', 'yip yip');
+const vera = new Pet('dog', 7, 'border collie', 'woof woof');
 
 console.log(ernie);
 console.log(vera);
+
+ernie.speak();
+vera.speak();
