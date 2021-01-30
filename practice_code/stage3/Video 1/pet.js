ef0399c4c4d2d54ab77b8455ced0c750fe51760c
Michael Vautour <michael.e.vautour@gmail.com>
2021-01-30 14:39:32 -0700

    create variable using the constructor and outputted the results to console.log

--- practice_code/stage3/Video 1/pet.js
+++ practice_code/stage3/Video 1/pet.js
@@ -5,3 +5,9 @@ class Pet {
         this.breed = breed;
     }
 }
+
+const ernie = new Pet('dog', 2, 'pug');
+const vera = new Pet('dog', 7, 'border collie');
+
+console.log(ernie);
+console.log(vera);
